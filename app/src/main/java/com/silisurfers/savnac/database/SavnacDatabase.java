96f534a5d54aca8df54b3c6169309424ce49fc6d
vmc0823 <vwaltman@csumb.edu>
2025-04-13 15:00:44 -0700

    course class created, might represent courses created by teachers

--- app/src/main/java/com/silisurfers/savnac/database/SavnacDatabase.java
+++ app/src/main/java/com/silisurfers/savnac/database/SavnacDatabase.java
@@ -1,8 +1,14 @@
 package com.silisurfers.savnac.database;
 
+import android.content.Context;
+
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.silisurfers.savnac.database.entities.SavnacGradeEntry;
 import com.silisurfers.savnac.database.typeConverters.DateConverter;
@@ -12,4 +18,26 @@ import com.silisurfers.savnac.database.typeConverters.DateConverter;
 public abstract class SavnacDatabase extends RoomDatabase {
 
     //TODO: set fields, synchronize dao
+
+
+
+    private static volatile SavnacGradeEntry INSTANCE;
+
+//    static SavnacDatabase getDatabase(final Context context) {
+//        if (INSTANCE == null) {
+//            synchronized (SavnacDatabase.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+//                                    SavnacDatabase.class, "Savnac")
+//                            .fallbackToDestructiveMigration()
+//                            .addCallback(addDefaultValues)
+//                            .build();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+//    }
+
 }
+
+
