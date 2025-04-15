3fcdebb62fec7366fa5dc803f468f2f7083ac2ab
Brandon Evans <0595069@my.msjc.edu>
2025-04-15 00:54:37 -0600

    Implemented Login and Signup activity class stubs

---
+++ app/src/main/java/com/silisurfers/savnac/LoginActivity.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Brandon Evans
+ * File: LoginActivity.java
+ * Date: 4/15/2025
+ * Description:
+ */
+
+package com.silisurfers.savnac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    // TODO: implement LoginActivity class
+}
