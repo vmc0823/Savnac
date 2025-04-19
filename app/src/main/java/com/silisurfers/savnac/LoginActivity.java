729d4a350bd67894909a2559386db46f98b4f856
Brandon Evans <0595069@my.msjc.edu>
2025-04-19 15:23:03 -0600

    Added stubs for LoginActivity.java class methods

--- app/src/main/java/com/silisurfers/savnac/LoginActivity.java
+++ app/src/main/java/com/silisurfers/savnac/LoginActivity.java
@@ -7,8 +7,33 @@
 
 package com.silisurfers.savnac;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.silisurfers.savnac.database.SavnacDatabase;
+import com.silisurfers.savnac.database.SavnacUserDao;
+
 public class LoginActivity extends AppCompatActivity {
-    // TODO: implement LoginActivity class
+    // Private data -------------------------------------------------------------------------------
+    private SavnacUserDao userDAO;
+
+    private static SavnacDatabase repository;
+
+    // Class behaviors ----------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: Finish onCreate method
+    }
+
+    private void verifyUser() {
+        // TODO: Implement verifyUser method
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginActivity.class);
+    }
 }
