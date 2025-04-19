d995b4b1bb302456db8d2ee3a56bb603afb4079d
TomKestrel <wootarkkim@gmail.com>
2025-04-18 23:34:51 -0700

    Progress update (18 April 2024): added a java activity page. Forgot to create a branch. Will merge this and then create a branch to work further on it.

---
+++ app/src/main/java/com/silisurfers/savnac/join_or_leave_courses_teacher_perspective_activity.java
@@ -0,0 +1,8 @@
+package com.silisurfers.savnac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class join_or_leave_courses_teacher_perspective_activity extends AppCompatActivity {
+
+
+}
