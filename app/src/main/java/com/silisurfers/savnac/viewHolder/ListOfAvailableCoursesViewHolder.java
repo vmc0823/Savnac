a1b491cc7dc545626b8b66cf4dfbd23abec736af
TomKestrel <wootarkkim@gmail.com>
2025-04-19 00:10:50 -0700

    Progress update (19 April 2024): made a viewHolder file. Started learning how recyclerView works

---
+++ app/src/main/java/com/silisurfers/savnac/viewHolder/ListOfAvailableCoursesViewHolder.java
@@ -0,0 +1,14 @@
+package com.silisurfers.savnac.viewHolder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListOfAvailableCoursesViewHolder  extends RecyclerView.ViewHolder {
+
+
+    public ListOfAvailableCoursesViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
