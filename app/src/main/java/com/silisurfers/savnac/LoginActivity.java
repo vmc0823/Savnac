e8ccfb417f6441e6ba90257a44ce888cda0eff81
Brandon Evans <0595069@my.msjc.edu>
2025-04-20 12:56:11 -0600

    Implemented verifyUser might change how this works later

--- app/src/main/java/com/silisurfers/savnac/LoginActivity.java
+++ app/src/main/java/com/silisurfers/savnac/LoginActivity.java
@@ -11,11 +11,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
 import com.silisurfers.savnac.database.SavnacDatabase;
 import com.silisurfers.savnac.database.SavnacRepository;
+import com.silisurfers.savnac.database.SavnacUserDao;
+import com.silisurfers.savnac.database.entities.SavnacUser;
 import com.silisurfers.savnac.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -36,13 +40,36 @@ public class LoginActivity extends AppCompatActivity {
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // IMPLEMENT VERIFYUSER
+                verifyUser();
             }
         });
     }
 
     private void verifyUser() {
-        // TODO: Implement verifyUser method
+        String username = binding.emailLoginEditText.getText().toString();
+
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Username may not be blank.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO: Implement getUserByUserName method
+        LiveData<SavnacUser> userObserver = repository.getUserByUserName(username);
+        userObserver.observe(this, user -> {
+            if (user != null) {
+                String password = binding.passwordLoginEditText.getText().toString();
+                if (password.equals(user.getPassword())) {
+                    // Load course homepage if username and password match
+                    Intent intent = new Intent(this, CoursesActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Invalid password.", Toast.LENGTH_SHORT).show();
+                    binding.passwordLoginEditText.setSelection(0);
+                }
+            } else {
+                Toast.makeText(this, String.format("%s is not a valid username.", username), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     static Intent loginIntentFactory(Context context) {
