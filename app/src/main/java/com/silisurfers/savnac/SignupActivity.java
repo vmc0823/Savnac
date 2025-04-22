e40276deaf0b28199f03411709aa9c68f826a08d
Brandon Evans <0595069@my.msjc.edu>
2025-04-22 13:09:28 -0600

    Added method stubs for SignupActivity.java

--- app/src/main/java/com/silisurfers/savnac/SignupActivity.java
+++ app/src/main/java/com/silisurfers/savnac/SignupActivity.java
@@ -7,8 +7,33 @@
 
 package com.silisurfers.savnac;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.silisurfers.savnac.database.SavnacRepository;
+import com.silisurfers.savnac.databinding.ActivityLoginBinding;
+
 public class SignupActivity extends AppCompatActivity {
-    // TODO: implement SignupActivity class
+    // Private data -------------------------------------------------------------------------------
+    private ActivityLoginBinding binding;
+
+    private SavnacRepository repository;
+
+    // Class behaviors ----------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: implement onCreate
+    }
+
+    private void setupUser() {
+        // TODO: implement setupUser
+    }
+
+    static Intent signupIntentFactory(Context context) {
+        return new Intent(context, SignupActivity.class);
+    }
 }
