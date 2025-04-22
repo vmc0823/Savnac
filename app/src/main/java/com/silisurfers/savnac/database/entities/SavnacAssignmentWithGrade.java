256fdfac5ba3b7dff00065eaa641a4883e0ba868
vmc0823 <vwaltman@csumb.edu>
2025-04-22 15:01:38 -0700

    added index, fixed constructor in assignments/with grade entities

--- app/src/main/java/com/silisurfers/savnac/database/entities/SavnacAssignmentWithGrade.java
+++ app/src/main/java/com/silisurfers/savnac/database/entities/SavnacAssignmentWithGrade.java
@@ -1,5 +1,6 @@
 package com.silisurfers.savnac.database.entities;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Embedded;
 import androidx.room.Ignore;
 
@@ -7,22 +8,35 @@ public class SavnacAssignmentWithGrade {
     @Embedded
     public SavnacGradeEntry grade;
 
-    @Embedded(prefix = "course_")
-    public SavnacCourse course;
+//    @Embedded(prefix = "course_")
+//    public SavnacCourse course;
 
     @Embedded(prefix = "course_assignment")
     public SavnacAssignment assignment;
 
-    public SavnacAssignmentWithGrade(){
+    @ColumnInfo(name = "assignment_name")
+    public String assignmentName;
 
+    @ColumnInfo(name = "points")
+    public int points;
+
+    public SavnacAssignmentWithGrade(SavnacGradeEntry grade, int points, String assignmentName, SavnacAssignment assignment) {
+        this.grade = grade;
+        this.points = points;
+        this.assignmentName = assignmentName;
+        this.assignment = assignment;
     }
 
-    @Ignore
-    public SavnacAssignmentWithGrade(String assignmentName, int assignmentPoints, int grade){
-        this.assignment = new SavnacAssignment();
-        this.grade = new SavnacGradeEntry();
-        this.assignment.setAssignmentName(assignmentName);
-        this.assignment.setPoints(assignmentPoints);
-        this.grade.setGrade(grade);
-    }
+    //    public SavnacAssignmentWithGrade(){
+//
+//    }
+//
+//    @Ignore
+//    public SavnacAssignmentWithGrade(String assignmentName, int assignmentPoints, int grade){
+//        this.assignment = new SavnacAssignment();
+//        this.grade = new SavnacGradeEntry();
+//        this.assignment.setAssignmentName(assignmentName);
+//        this.assignment.setPoints(assignmentPoints);
+//        this.grade.setGrade(grade);
+//    }
 }
