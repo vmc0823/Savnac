a16cad0bc087819f7a7242c6015862a0595427f3
vmc0823 <vwaltman@csumb.edu>
2025-04-22 22:58:26 -0700

    syncing CoursesActivity to db

--- app/src/main/java/com/silisurfers/savnac/CoursesActivity.java
+++ app/src/main/java/com/silisurfers/savnac/CoursesActivity.java
@@ -30,6 +30,7 @@ public class CoursesActivity extends AppCompatActivity {
     private CoursesActivityRecyclerAdapter adapter;
     private SavnacRepository repo;
     private SavnacUser currentUser;
+    private Button createNewCourseButton;
 
 
 
@@ -43,20 +44,21 @@ public class CoursesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        repo = SavnacRepository.getInstance(getApplication());
-        currentUser = repo.getCurrentUser().getValue();
+        repo = SavnacRepository.getInstance(getApplicationContext());
+//        currentUser = repo.getCurrentUser().getValue();
         recyclerView = findViewById(R.id.courses_recycler_view);
+        createNewCourseButton = findViewById(R.id.create_a_course_button);
 
         ///  just some dummy data for now
-        courses = new ArrayList<>();
-        courses.add(new SavnacCourse("CST 300", 1));
-        courses.add(new SavnacCourse("CST 338", 1));
-        courses.add(new SavnacCourse("CST 363", 1));
+//        courses = new ArrayList<>();
+//        courses.add(new SavnacCourse("CST 300", 1));
+//        courses.add(new SavnacCourse("CST 338", 1));
+//        courses.add(new SavnacCourse("CST 363", 1));
 
 //        adapter = new CoursesActivityRecyclerAdapter(courses);
 
         // added by Tom (19 April 2025)
-        adapter = new CoursesActivityRecyclerAdapter(courses, course ->{
+        adapter = new CoursesActivityRecyclerAdapter(new ArrayList<>(), course -> {
             // JAVA forces this 2nd parameter to exist but it doesn't do anything
             // this is because the changes ive modified for the CoursesActivityRecyclerAdapter was meant for my
             // joinOrLeaveCoursesTeacherPerspectiveActivity activity page (not this coursesActivity... yet)
@@ -65,17 +67,23 @@ public class CoursesActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
-        Button createNewCourseButton = findViewById(R.id.create_a_course_button);
-        if(currentUser == null || !"teacher".equals(currentUser.getRole())) {
-            createNewCourseButton.setVisibility(View.GONE);
-        }
-
-
-        createNewCourseButton.setOnClickListener(view -> {
-            Intent intent = new Intent(this, CreateCourseActivity.class);
-            startActivity(intent);
+        repo.getCurrentUser().observe(this, user -> {
+            if (user != null && "teacher".equals(user.getRole())) {
+                createNewCourseButton.setVisibility(View.VISIBLE);
+                repo.getCourseByTeacher(user.getId())
+                        .observe(this, coursesList -> {
+                            adapter.updateData(coursesList);
+                        });
+            } else {
+                createNewCourseButton.setVisibility(View.GONE);
+                repo.getAllCourses().observe(this, coursesList ->
+                        adapter.updateData(coursesList));
+            }
         });
 
 
+        createNewCourseButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, CreateCourseActivity.class));
+        });
     }
 }
